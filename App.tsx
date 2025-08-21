d2a28b1f1283d4d9ca8ea50f7242b41de2152b9e
michaelscottletzelter-afk <226945551+michaelscottletzelter-afk@users.noreply.github.com>
2025-08-20 22:01:18 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>AutoForm Pro</h1>
+      <p>Welcome to your automation platform!</p>
+    </div>
+  );
+}
+
+export default App;
